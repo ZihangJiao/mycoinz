27dd1d6ca413ba3009fdb2d1066b72cfe60c00df
jiaozihang <s1622520@sms.ed.ac.uk>
2018-12-01 22:42:52 +0000

    sth

--- app/src/main/java/com/example/jiaozihang/coinz/MainActivity.kt
+++ app/src/main/java/com/example/jiaozihang/coinz/MainActivity.kt
@@ -3,26 +3,71 @@ package com.example.jiaozihang.coinz
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.annotation.NonNull
 import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.*
 import kotlinx.android.synthetic.main.activity_main.*
 
 
-
 class MainActivity : AppCompatActivity() {
+    val mAuth = FirebaseAuth.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        ready.setOnClickListener {
-            Log.i("MathActivity", "Botton was clicked !")
-            Toast.makeText(this, "Welcome to Coinz !", Toast.LENGTH_SHORT).show()
-            val intent = Intent(this,SecondActivity::class.java)
-            startActivity(intent)
+        var loginBtn = findViewById<View>(R.id.loginBtn) as Button
+        loginBtn.setOnClickListener(View.OnClickListener{
+            view -> login()
 
-
-        }
+        })
 
     }
+
+
+    override fun onStart() {
+        super.onStart()
+        if(mAuth.currentUser == null){
+            startActivity(Intent(this,PhoneAuthCredential::class.java))
+        }else{
+            Toast.makeText(this,"Already signed in:)",Toast.LENGTH_LONG).show()
+        }
+    }
+
+
+
+
+
+    private fun login(){
+        val emailTxt = findViewById<View>(R.id.emailTxt) as EditText
+        val passwordTXT = findViewById<View>(R.id.passwordTxt) as EditText
+
+        var email = emailTxt.text.toString()
+        var password = passwordTXT.text.toString()
+
+        if(!email.isEmpty() && !password.isEmpty()){
+            mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(this, OnCompleteListener { task ->
+                if(task.isSuccessful){
+                    startActivity(Intent(this,SecondActivity::class.java))
+                    Toast.makeText(this,"Successfully logged in :)", Toast.LENGTH_LONG).show()
+                }
+            })
+        }else{
+            Toast.makeText(this,"Please fill up the credentials :(",Toast.LENGTH_LONG).show()
+        }
+    }
+
+
+
+
+
+
 }
